7524a3d59dacfaa61b90cc0bfac35be33d9af6ca
ODESNILDO FARIAS NOGUEIRA <ODESNILDO.ALUNOS@CFP127ALUNO.LOCAL>
2023-05-27 11:49:23 -0300

    Dalo

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <p>Welcome to the Home page!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+};
+</script>
+
+<style scoped>
+/* Estilos específicos para o componente Home.vue */
+</style>
