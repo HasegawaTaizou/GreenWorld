6f777dd161894e2c3f3e481043b226cf5a75fd52
HasegawaTaizou <caiocoghi@gmail.com>
2023-05-30 22:25:33 -0300

    index page done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <nav class="navigation">
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link> -->
     <router-view></router-view>
-  </nav>
 </template>
